563fb3c9ccc9a05e9a458eb29cf75f09ca87b609
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-08 19:58:26 -0700

    initial commit

---
+++ src/sorting/bubble_sort/bubble_sort.js
@@ -0,0 +1,13 @@
+const bubbleSort = (unsortedArray) => {
+  const sortedArray = [...unsortedArray];
+
+  for (let i = 0; i < sortedArray.length - 1; i += 1) {
+    if (sortedArray[i] > sortedArray[i + 1]) {
+      [sortedArray[i], sortedArray[i + 1]] = [sortedArray[i + 1], sortedArray[i]];
+    }
+  }
+
+  return sortedArray;
+};
+
+export default bubbleSort;
