a1527b1f3134c7759d88d429456d4095739eb4c6
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-09 23:51:14 -0700

    Adding insertion sort

---
+++ src/sorting/insertion_sort/insertion_sort.js
@@ -0,0 +1,17 @@
+const insertionSort = (unsortedArr) => {
+  const sortedArr = [...unsortedArr];
+
+  for (let i = 1; i < sortedArr.length; i += 1) {
+    if (sortedArr[i] < sortedArr[i - 1]) {
+      let currElement = i;
+      for (let j = i - 1; j >= 0; j -= 1) {
+        if (sortedArr[currElement] < sortedArr[j]) {
+          [sortedArr[currElement], sortedArr[j]] = [sortedArr[j], sortedArr[currElement]];
+          currElement = j;
+        }
+      }
+    }
+  }
+};
+
+export default insertionSort;
