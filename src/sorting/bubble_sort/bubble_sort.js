3ba666b775b69c8880558a7a712ca4d8bcdc3f2e
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-10 10:29:43 -0700

    Change from AirBnB linter to Standard

--- src/sorting/bubble_sort/bubble_sort.js
+++ src/sorting/bubble_sort/bubble_sort.js
@@ -1,13 +1,13 @@
 const bubbleSort = (unsortedArray) => {
-  const sortedArray = [...unsortedArray];
+  const sortedArray = [...unsortedArray]
 
   for (let i = 0; i < sortedArray.length - 1; i += 1) {
     if (sortedArray[i] > sortedArray[i + 1]) {
-      [sortedArray[i], sortedArray[i + 1]] = [sortedArray[i + 1], sortedArray[i]];
+      [sortedArray[i], sortedArray[i + 1]] = [sortedArray[i + 1], sortedArray[i]]
     }
   }
 
-  return sortedArray;
-};
+  return sortedArray
+}
 
-export default bubbleSort;
+export default bubbleSort
--- src/sorting/insertion_sort/insertion_sort.js
+++ src/sorting/insertion_sort/insertion_sort.js
@@ -1,17 +1,17 @@
 const insertionSort = (unsortedArr) => {
-  const sortedArr = [...unsortedArr];
+  const sortedArr = [...unsortedArr]
 
   for (let i = 1; i < sortedArr.length; i += 1) {
     if (sortedArr[i] < sortedArr[i - 1]) {
-      let currElement = i;
+      let currElement = i
       for (let j = i - 1; j >= 0; j -= 1) {
         if (sortedArr[currElement] < sortedArr[j]) {
-          [sortedArr[currElement], sortedArr[j]] = [sortedArr[j], sortedArr[currElement]];
-          currElement = j;
+          [sortedArr[currElement], sortedArr[j]] = [sortedArr[j], sortedArr[currElement]]
+          currElement = j
         }
       }
     }
   }
-};
+}
 
-export default insertionSort;
+export default insertionSort
