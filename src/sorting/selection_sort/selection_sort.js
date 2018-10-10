67435fa168c17c4365cf6537774ddbea8323c23e
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-10 14:54:14 -0700

    Adding selection sort

---
+++ src/sorting/selection_sort/selection_sort.js
@@ -0,0 +1,17 @@
+const selectionSort = (unsortedArr) => {
+  const sortedArr = [...unsortedArr]
+
+  for (let i = 0; i < sortedArr.length - 1; i++) {
+    let indexOfMinVal = i
+    for (let j = i + 1; j < sortedArr.length; j++) {
+      if (sortedArr[j] < sortedArr[indexOfMinVal]) {
+        indexOfMinVal = j
+      }
+    }
+    if (indexOfMinVal !== i) {
+      [sortedArr[i], sortedArr[indexOfMinVal]] = [sortedArr[indexOfMinVal], sortedArr[i]]
+    }
+  }
+}
+
+export default selectionSort
